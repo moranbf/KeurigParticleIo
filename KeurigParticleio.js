4910ae55fc536bf2964bd39f8bf9e587d20384a6
moranbf <moranbf@gmail.com>
2017-01-02 21:44:56 -0500

    Update KeurigParticleio.js
    Made some changes to SetTime()

--- KeurigParticleio.js
+++ KeurigParticleio.js
@@ -17,23 +17,35 @@ int powerLedIn = D1;
 int btnsIn = A1;
 // The wire to the main control unit will be the output and wired to the DAC
 int btnsOut = DAC;
-// All of the buttons on the Kuerig are wired to one lead on the main control board
+// SplicE into the Add Water Sensor, there are two black lines, one sends 5v to the magnetic sensor the other
+// is at +5v when there is water
+int addWater = D3;
+
+int heaterVal = A3;
+int dataPin = D2;
+int toggle =0;
+int toggleLoop = 0;
+// All of the buttons (pwr, lrg, med, sml, menu) on the Kuerig are wired to one lead on the main control board
 // Resistors bump the voltage up, or to ground for toggling power, and it appears a ADC on the main control unit read 
 // different voltages for different buttons, so lets define them
 // divide voltage by 3.3 and multiply by 4095 to get the value to set the DAC to
 // dac = voltabe/3.3 * 4095
-int dacOn = 2.4/3.3 * 4095;
-int dacOff = 0.0;
-int dacMedium = 2.6/3.3 * 4095;
-int dacSmall =  2.8/3.3 * 4095;
-int dacLarge = 4095;
+int dacOn = (3.3 - 2.4)/3.3 * 4095;
+int dacMenu = (3.3 - 2.5)/3.3  * 4095 ;
+int dacOff = 4095;
+int dacMedium = (3.3 - 2.6)/3.3  * 4095;
+int dacSmall =  (3.3 - 2.8)/3.3  * 4095;
+int dacLarge = 0;
 
 void setup() {
 pinMode(btnsOut,OUTPUT);
 pinMode(btnsIn,INPUT);
 pinMode(kcupClosedOut, OUTPUT);
+pinMode(heaterVal, INPUT);
 pinMode(powerLedIn, INPUT_PULLDOWN);
 pinMode(kcupClosedIn, INPUT_PULLDOWN);
+pinMode(addWater,INPUT_PULLDOWN);
+pinMode(dataPin,INPUT_PULLDOWN);
 Particle.function("powerOn",powerOn);
 Particle.function("powerOff",powerOff);
 Particle.function("powerState",powerState);
@@ -41,18 +53,24 @@ Particle.function("brew",brew);
 Particle.function("dac_out",dac_out);
 Particle.function("read_value",read_value);
 Particle.function("power",power);
+Particle.function("heaterCheck",heaterCheck);
+Particle.function("waterCheck",waterCheck);
+Particle.function("checkDataPin",checkDataPin);
 // setup the brew/power/menu line to a steady state which is aprox 2.4v when plugged in
 analogWrite(btnsOut, dacOn);
+// Set the Time on the keurig clock using the menu, sml, and lrg buttons
+setTime();
+Particle.publish("Setup finished");
 }
 
 void loop() {
-    // the main loop will capture when a button is pressed and brew the correct size
-    int newButtonValue = analogRead(btnsIn);
+    //the main loop will capture when a button is pressed and brew the correct size
+      int newButtonValue = analogRead(btnsIn);
     // value range for when the large button is pressed
     if (newButtonValue < 4100 && newButtonValue > 3900){
-        brew("large");
+       brew("large");
     }
-    // value range for when the medium button is pressed
+   //  value range for when the medium button is pressed
     else if (newButtonValue < 3300 && newButtonValue > 3100){
         brew("medium");
     }
@@ -60,11 +78,65 @@ void loop() {
     else if (newButtonValue < 3500 && newButtonValue > 3350){
         brew("small");
     }
-    // value range for when the power button is pressed
+   //  value range for when the power button is pressed
     else if (newButtonValue < 100){
         togglePower();
     }
+    
 }
+
+int dataCheck(){
+   if (toggle == 0){
+       toggle = 1;
+   }
+   else if (toggle == 1){
+       toggle = 0;
+   }
+}
+
+int checkDataPin(String blah){
+    int value = digitalRead(dataPin);
+    return toggle;
+}
+int heaterCheck(String blah){
+    return analogRead(heaterVal);
+}
+
+// Set the Time on the Unit using the menu, lrg and sml buttons
+int setTime(){
+    int i;
+    // Wait a minute make sure the display PIC is up
+    delay(10000);
+    int powerStatus = powerState("a");
+    if ( powerStatus == 0){
+        powerOn("a");
+    }
+    delay(2000);
+    // Set Timezone, get the our hours in miltary format and minutes
+    Time.zone(-6);
+    int hour = Time.hour();
+    int minute = Time.minute();
+    // Hit the menu button, this should get you to the time set option
+    // the sml button will increment hours, lrg minutes
+    menu();
+    for (i = 0; i < minute; i++) {
+    // Loop through once for minutes hitting the lrg button
+        lrg(); 
+    }
+    // Loop through once for hours hitting the sml button
+    for (i = 0; i < hour; i++) {
+        sml();
+    }
+    // Hit the menu button 5 more times to get out of menu mode
+    menu();
+    menu();
+    menu();
+    menu();
+    menu();
+    // power down
+    powerOff("a");
+    return 0;
+    }
 
 // Toggle the power, by setting btnsOut to ground and back to dacOn
 int togglePower() {
@@ -72,6 +144,9 @@ int togglePower() {
          delay(300);
          analogWrite(btnsOut, dacOn);
          return 0;
+}
+int waterCheck(String command){
+    return digitalRead(addWater);
 }
 
 int power(String onOff) {
@@ -131,6 +206,29 @@ int powerOff(String command) {
     }
 }
 
+// Functions used to set the time
+int menu(){
+    delay(300);
+    analogWrite(btnsOut, dacMenu); 
+    delay(500);
+    analogWrite(btnsOut, dacOn); 
+    delay(300);
+    return 0;
+}
+int lrg(){
+    analogWrite(btnsOut, dacLarge); 
+    delay(100);
+    analogWrite(btnsOut, dacOn); 
+    delay(100);
+    return 0;
+}
+int sml(){
+    analogWrite(btnsOut, dacSmall); 
+    delay(100);
+    analogWrite(btnsOut, dacOn); 
+    delay(100);
+    return 0;
+}
 int brew(String cSize){
     // First make sure the kcup handle is closed if so toggle the sensor
     int kcupState = digitalRead(kcupClosedIn);
@@ -144,6 +242,10 @@ int brew(String cSize){
     else {
         return -1;
     }
+    if (waterCheck("a") == 0){
+        return -999;
+    }
+
     if (cSize.equals("large")){
         analogWrite(btnsOut, dacLarge); 
         delay(300);
@@ -168,10 +270,11 @@ int brew(String cSize){
 }
 
 // Diagnostic Function, Suply a voltage and the DAC will output it
-int dac_out(String voltage){
-    float f_volt = voltage.toFloat();
-    float val = f_volt/3.3 * 4095;
-    analogWrite(DAC1, val);
+int dac_out(String val){
+    //float f_volt = voltage.toFloat();
+    //float val = f_volt/3.3 * 4095;
+    analogWrite(btnsOut, val.toInt());
+
     return 0;
 }
 // Diagnostic Function, Read the values when a button is pressed 
@@ -196,3 +299,4 @@ int powerState(String command) {
         return 0;
     }
 }
+
